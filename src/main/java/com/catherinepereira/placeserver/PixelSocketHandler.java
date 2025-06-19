c32007dfab72227ba426feb5c541e68acb1e7c11
Cat Pereira <catherinelpa@gmail.com>
2025-06-19 14:41:18 -0700

    Add validation of client messages, tiny bit of progress on packing color data for new connections

--- src/main/java/com/catherinepereira/placeserver/PixelSocketHandler.java
+++ src/main/java/com/catherinepereira/placeserver/PixelSocketHandler.java
@@ -1,6 +1,7 @@
 package com.catherinepereira.placeserver;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -20,54 +21,43 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class PixelSocketHandler extends BinaryWebSocketHandler {
     private final int width;
     private final int height;
-    private final String[][] board;
-
-    public record PlaceMessage(@JsonProperty("x") int x, @JsonProperty("y") int y, @JsonProperty("color") String color) {}
+    private final int paletteIndexLimit = 15;
+    private final int[][] board;
 
     private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
     private static final Logger logger = LoggerFactory.getLogger(PixelSocketHandler.class);
 
     public PixelSocketHandler(int width, int height) {
-        this.board = new String[width][height];
+        this.board = new int[width][height];
         this.width = width;
         this.height = height;
     }
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws IOException {
+    public void afterConnectionEstablished(WebSocketSession session) {
         logger.debug("Client connected: {}", session.getId());
         sessions.add(session);
 
-        // Replicate board to new client
-        var objectMapper = new ObjectMapper();
+//        byte[] packedArray = new byte[width * height / 2];
+//
+//        for (int x = 0; x < this.width; x++) {
+//            for (int y = 0; y < this.height; y += 2) {
+//                int currentColor = this.board[x][y];
+//                var nextColor = this.board[x][y + 1];
+//
+///               // Handle case where board size is odd and last color byte in array is undefined
+//
+//                int currentPackedColor = currentColor << 4;
+//                int nextPackedColor = currentPackedColor | nextColor;
+//
+//                byte[] byteArray = ByteBuffer.allocate(2).put((byte) currentPackedColor).put((byte) nextPackedColor).array();
+//                packedArray.push(byteArray);
+//
+//            }
+//        }
 
-        String[] batchedMessages = new String[width * height];
-
-        for (int x = 0; x < board.length; x++) {
-            for (int y = 0; y < board[x].length; y++) {
-                String color = board[x][y];
-                if (color == null) {
-                    continue;
-                }
-
-                var placeMessage = new PlaceMessage(x, y, color);
-                String json;
-                try {
-                    json = objectMapper.writeValueAsString(placeMessage);
-                } catch (IOException e) {
-                    logger.error(e.getMessage());
-                    return;
-                }
-                batchedMessages[y * height + x] = json;
-            }
-        }
-
-        String[] nonNullMessages = Arrays.stream(batchedMessages).filter(Objects::nonNull).toArray(String[]::new);
-        logger.debug("Batched message: {} ", (Object) nonNullMessages);
-
-        String message = Arrays.toString(nonNullMessages);
-        session.sendMessage(new TextMessage(message));
+//        session.sendMessage();
     }
 
     @Override
@@ -76,8 +66,29 @@ public class PixelSocketHandler extends BinaryWebSocketHandler {
     }
 
     @Override
-    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
-        System.out.println("Message payload length" + message.getPayloadLength());
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws RuntimeException {
+        var payload = message.getPayload();
+        var duplicate = payload.duplicate();
+
+        var x = duplicate.getShort(0); // Reads x coord at byte index 0
+        var y = duplicate.getShort(2); // Reads y coord at byte index 2
+        var color = duplicate.get(4); // Reads color at byte index 4
+
+        // Check coords are within bounds
+        if (x > this.width) {
+            System.out.println("x is out of bounds");
+            return;
+        }
+        if (y > this.height) {
+            System.out.println("y is out of bounds");
+            return;
+        }
+        if (color > this.paletteIndexLimit) {
+            System.out.println("Color is out of bounds");
+            return;
+        }
+
+        board[x][y] = color;
 
         for (WebSocketSession s : sessions) {
             if (s.isOpen()) {
